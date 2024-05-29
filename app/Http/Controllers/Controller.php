453ca3c07ad4874267834f210089596938b638eb
Amos Marbun <amozmarboen02@gmail.com>
2024-05-29 08:36:33 +0700

    mengubah form select bororw book, select book, dan index borrow book

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Curriculum;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -9,5 +10,10 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+    protected $defaultCurriculum ;
+    public function __construct() {
+        //membuat global variable, curriculum yang default/sedang berjalan
+        $this->defaultCurriculum = Curriculum::where("is_default", "1")->first();
+    }
 }
 
--- resources/views/library/borrow/borrow_form.blade.php
+++ resources/views/library/borrow/borrow_form.blade.php
@@ -20,7 +20,7 @@
                 <option value="">Select Patron</option>
                 @foreach ($students as $student)
                     <option value="{{ $student->id }}" {{ (isset($borrow) && $borrow->student_id == $student->id) ? 'selected' : '' }}>
-                        {{ $student->id }} - {{ $student->name }}
+                        {{ $student->classroom_name }} - {{ $student->identity_number }}  - {{ $student->name }}
                     </option>
                 @endforeach
             </select>
@@ -69,28 +69,25 @@
 @endif --}}
 
 <div class="row">
-    <div class="col-md-5 border">
-        <div class="row">
-            @foreach($books as $book)
-            <div class="col-md-4 mb-4 d-flex align-items-stretch mt-2">
-                <div class="card" style="width: 100%;">
-                    <img class="card-img-top card-img-fixed" src="{{ asset('storage/' . $book->image) }}" alt="Image Menu">
-                    <div class="card-body d-flex flex-column">
-                        <p>{{ $book->title }}</p>
-                        <form action="{{ route('book-borrow-detail.store') }}" method="POST" autocomplete="off" class="mt-auto">
-                            @csrf
-                            <input type="hidden" name="borrowing_book_id" value="{{ $borrow->id }}">
-                            <input type="hidden" name="book_id" value="{{ $book->id }}">
-                            <button type="submit" class="btn btn-primary btn-block">Select</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            @endforeach
-        </div>
-    </div>
+    {{-- <div class="col-md-5 border">
+        <div class="row"> --}}
+        <form action="{{ route('book-borrow-detail.store') }}" method="POST" autocomplete="off" class="mt-4">
+            @csrf
+            <input type="hidden" name="borrowing_book_id" value="{{ $borrow->id }}">
+            <select name="book_id" id="book_id" class="form-control data-select-2">
+                <option value="">ISBN - Cateogry - Title</option>
+                @foreach ($books as $book)
+                    <option value="{{ $book->id }}">
+                        {{ $book->isbn }} - {{ $book->category->name }}  - {{ $book->title }}
+                    </option>
+                @endforeach
+            </select>
+            <button type="submit" class="btn btn-primary btn-block">Select</button>
+        </form>
+        {{-- </div> --}}
+    {{-- </div> --}}
 
-    <div class="col-md-7 border ">
+    <div class="col-md-12 mt-5">
         {{-- <table class="table table-bordered table-striped mt-2 table-responsive"> --}}
         <table id="example" class="table table-striped table-bordered">
             <thead>
