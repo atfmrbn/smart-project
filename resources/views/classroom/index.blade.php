d8c918ab5849a00e995c02654ec020669192325d
debisafa <debisafa21@gmail.com>
2024-05-30 10:57:15 +0700

    ubah template

--- resources/views/classroom/index.blade.php
+++ resources/views/classroom/index.blade.php
@@ -19,7 +19,7 @@
             <thead>
                 <tr>
                     <th width="5%">No.</th>
-                    <th>Classroom Type ID</th>
+                    <th class="text-center">Classroom Type ID</th>
                     <th>Classroom Type Name</th>
                     <th>Name</th>
                     <th width="10%">Action</th>
@@ -29,21 +29,22 @@
                 @foreach ($classrooms as $index => $classroom)
                     <tr>
                         <td class="align-middle">{{ $index + 1 }}</td>
-                        <td class="align-middle">{{ $classroom->classroom_type_id }}</td>
+                        <td class="align-middle text-center">{{ $classroom->classroom_type_id }}</td>
                         <td class="align-middle">{{ $classroom->classroom_type->name }}</td>
                         <td class="align-middle">{{ $classroom->name }}</td>
                         <td>
                             <div class="d-flex">
-                                <a href="{{ URL::to('classroom/' . $classroom->id) }}"
-                                    class="btn btn-sm btn-info mr-2">Show</a>
-                                <a href="{{ URL::to('classroom/' . $classroom->id) . '/edit' }}"
-                                    class="btn btn-sm btn-warning mr-2">Edit</a>
+                                <a title="Lihat" href="{{ URL::to('classroom/' . $classroom->id) }}"
+                                    class="btn btn-sm btn-outline-info me-2"><i class="fas fa-eye"></i></a>
+                                <a href="{{ URL::to('classroom/' . $classroom->id . '/edit') }}"
+                                    class="btn btn-sm btn-outline-primary me-2"><i class="fas fa-edit"></i></a>
 
                                 <form action="{{ URL::to('classroom/' . $classroom->id) }}" method="post">
                                     @csrf
                                     @method('delete')
-                                    <button type="submit" class="btn btn-sm btn-danger"
-                                        onclick="return confirm('Anda yakin mau menghapus data ini {{ $classroom->name }} ?')">Delete</button>
+                                    <button type="submit" class="btn btn-sm btn-outline-danger me-2"
+                                        onclick="return confirm('Anda yakin mau menghapus data ini {{ $classroom->name }} ?')"><i
+                                            class="fas fa-trash"></i></button>
                                 </form>
                             </div>
                         </td>
