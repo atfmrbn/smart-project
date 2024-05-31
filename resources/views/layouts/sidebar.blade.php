2425d3cd887a6a4f9461e4093240b24bc70bea01
amosmrbn <136162455+amosmrbn@users.noreply.github.com>
2024-05-31 14:29:50 +0700

    Update sidebar me-nonaktifkan menu Extracurriculars

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -17,9 +17,9 @@
           <a href="#"><i class="fas fa-graduation-cap"></i> <span> Students</span> <span class="menu-arrow"></span></a>
           <ul>
             <li><a href="{{ URL::to('/student/student-list') }}">Student List</a></li>
-            <li><a href="{{ URL::to('/extracurricular') }}">Extracurriculars</a></li>
+<!--             <li><a href="{{ URL::to('/extracurricular') }}">Extracurriculars</a></li>
             <li><a href="add-student.html">Student Add</a></li>
-            <li><a href="edit-student.html">Student Edit</a></li>
+            <li><a href="edit-student.html">Student Edit</a></li> -->
           </ul>
         </li>
         <li class="submenu">
