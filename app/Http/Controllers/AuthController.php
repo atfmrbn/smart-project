9a5352a3956cab0e7d1977ee67650f48c21fd369
debisafa <debisafa21@gmail.com>
2024-06-05 21:58:06 +0700

    extracurricular edit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -24,6 +25,8 @@ class AuthController extends Controller
                 'username' => 'required',
                 'password' => 'required'
         ]);
+
+        // $data['password'] = Hash::make($request->password);
 
         if(Auth::attempt($data))
         {
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -44,7 +44,7 @@
               </div>
               <a class="dropdown-item" href="profile.html">My Profile</a>
               <a class="dropdown-item" href="inbox.html">Inbox</a>
-              <a class="dropdown-item" href="#">Logout</a>
+              <a class="dropdown-item" href="{{ route('logout') }}">Logout</a>
             </div>
           </li>
         </ul>
